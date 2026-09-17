be1c1cd9367efcd15de16ab731d71da5dc110c0f
Hussna_Majidi <hussnamajidi@gmail.com>
2026-09-17 22:10:17 +0430

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // Or whatever framework plugin you are using
+import react from '@vitejs/plugin-react' // (or whatever plugin you are using)
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Coffee/', // <--- THIS IS CRITICAL
+  base: '/Coffee/', // <--- MUST BE EXACTLY THIS, NOT A COMMENT
 })
